84fcea914a4fd781fafd713d319fc9cb92f80079
Alan Brennan <alan.brennan@shopify.com>
2019-06-11 11:47:57 +0100

    api removed

--- src/App.js
+++ src/App.js
@@ -8,13 +8,7 @@ class App extends Component {
   componentWillMount() {
     firebase.initializeApp(
       {
-        apiKey: 'AIzaSyAOMslnFL4_BxBjpKyXqFf4N4JKAY7BTeI',
-        authDomain: 'react-native-auth-1aba9.firebaseapp.com',
-        databaseURL: 'https://react-native-auth-1aba9.firebaseio.com',
-        projectId: 'react-native-auth-1aba9',
-        storageBucket: 'react-native-auth-1aba9.appspot.com',
-        messagingSenderId: '612137891086',
-        appId: '1:612137891086:web:37ba6c476f75f90a'
+        
       }
     );
   }
